4ffdc25dd872872c2ee181342e47e06c43d5b0f6
Mikhail Kim <kim.ms@ohystech.edu>
2026-06-03 23:29:40 +0300

    graceful shutdown for ctrl+c exit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from adapters.telegram.bot import run_bot
 
 if __name__ == "__main__":
-    run_bot()
+    print("Launching bot")
+    try:
+        run_bot()
+    except KeyboardInterrupt:
+        print("Graceful shutdown")
