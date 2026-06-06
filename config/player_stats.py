cd792601617daf25511dc0b467d1f58973e28e90
Mikhail Kim <kim.ms@ohystech.edu>
2026-06-06 15:45:59 +0300

    better

--- config/player_stats.py
+++ config/player_stats.py
@@ -2,4 +2,12 @@ from config.roles import ROLE_DESCRIPTIONS
 
 
 class PlayerRoleStats:
-    role_balance: dict[str, float] = {role: 0.0 for role in ROLE_DESCRIPTIONS.keys()}
+    def __init__(self, role_balance: dict[str, float] | None = None):
+        self.role_balance = {role: 0.0 for role in ROLE_DESCRIPTIONS.keys()}
+
+        if role_balance:
+            self.role_balance.update(role_balance)
+
+    @classmethod
+    def from_dict(cls, data: dict[str, float]) -> "PlayerRoleStats":
+        return cls(data)
