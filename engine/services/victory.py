8270aeb68e49f7924c4266e39db4c129537d2100
Mikhail Kim <kim.ms@ohystech.edu>
2026-06-08 22:15:50 +0300

    log victory cause

--- engine/services/victory.py
+++ engine/services/victory.py
@@ -6,6 +6,7 @@ from game_info.teams import Team
 
 from engine.game_state import Game, GameState
 
+from utils.logger import LOGGER
 from utils.win_rate_db import win_rate_database
 
 
@@ -16,6 +17,7 @@ async def check_victory(bus: EventBus, game: Game):
     alive = game.get_alive_players()
 
     if not alive:
+        LOGGER.verbose_debug("Everyone died: mafia wins")
         win_rate_database.update_win_rate_info(game, Team.MAFIA)
         response = ResponseBase(
             game.chat_id, "💀 Все умерли — победа мафии", valid=True
@@ -35,6 +37,7 @@ async def check_victory(bus: EventBus, game: Game):
     town = len(alive) - mafia - maniac
 
     if len(alive) <= 2 and maniac > 0:
+        LOGGER.verbose_debug("Maniac is left with the victim: maniac wins")
         win_rate_database.update_win_rate_info(game, Team.MANIAC)
         response = ResponseBase(
             game.chat_id,
@@ -46,6 +49,7 @@ async def check_victory(bus: EventBus, game: Game):
         return True
 
     if mafia == 0 and maniac == 0:
+        LOGGER.verbose_debug("Only citizens are left: citizens win")
         win_rate_database.update_win_rate_info(game, Team.CITIZEN)
         response = ResponseBase(
             game.chat_id,
@@ -57,6 +61,7 @@ async def check_victory(bus: EventBus, game: Game):
         return True
 
     if mafia >= town and maniac == 0:
+        LOGGER.verbose_debug("Mafia dominates table: mafia wins")
         win_rate_database.update_win_rate_info(game, Team.MAFIA)
         response = ResponseBase(
             game.chat_id,
